86adbfbd140c8751150e3268cbd26368a3426006
Respah-Nafula <nafularespah@gmail.com>
2022-03-04 15:07:27 +0300

    first commit

---
+++ src/main.kt
@@ -0,0 +1,26 @@
+fun main(){
+    var name="AkiraChix"
+    println(name[1].toString()+name[3]+name[4])
+    ageName()
+    string()
+    name
+    name()
+}
+fun ageName(){
+    var name='X'
+    var age= 'Y'
+    println("Hi my name is $name and I am $age years old")
+}
+fun string(){
+    var statement="coding in kotlin is very fun"
+    println(statement.length)
+}
+
+fun name(){
+    var identity="Respah Wakhungu"
+    if (identity=="Respah Wakhungu")
+        println("That's me")
+    else
+        println("I dont know who that is")
+    println(identity)
+}
